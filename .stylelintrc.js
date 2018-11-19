e983c880e389be18cf4d6b03f748e529d9ce1f66
auban <auban@stag-agency.be>
2018-11-19 16:33:33 +0100

    Add font

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -24,5 +24,13 @@ module.exports = {
         ],
       },
     ],
+    "at-rule-empty-line-before": [ "always", {
+        'except': [
+          'blockless-after-same-name-blockless',
+          'first-nested',
+        ],
+        'ignore': ['after-comment'],
+      }
+    ],
   },
 };
